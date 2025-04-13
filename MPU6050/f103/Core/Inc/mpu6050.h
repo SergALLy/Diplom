6908f7418e77a277a246f31c673ea9840546c349
SergALLy <cfyz70903@mail.ru>
2025-04-13 23:51:27 +0300

    v1.0

--- MPU6050/f103/Core/Inc/mpu6050.h
+++ MPU6050/f103/Core/Inc/mpu6050.h
@@ -1,61 +1,70 @@
 /*
  * mpu6050.h
  *
- *  Created on: Nov 13, 2019
- *      Author: Bulanov Konstantin
+ *  Created on: Apr 13, 2025
+ *      Author: SergALLy
  */
 
-#ifndef INC_GY521_H_
-#define INC_GY521_H_
+#ifndef INC_MPU6050_H_
+#define INC_MPU6050_H_
 
-#endif /* INC_GY521_H_ */
-
+#include "stdbool.h"
 #include <stdint.h>
-#include "i2c.h"
+#include "main.h"
 
-// MPU6050 structure
-typedef struct
-{
+typedef struct{
+	double X;
+	double Y;
+	double Z;
+} gyro_handle_t;
 
-    int16_t Accel_X_RAW;
-    int16_t Accel_Y_RAW;
-    int16_t Accel_Z_RAW;
-    double Ax;
-    double Ay;
-    double Az;
+typedef struct{
+	double X;
+	double Y;
+	double Z;
+} accel_handle_t;
 
-    int16_t Gyro_X_RAW;
-    int16_t Gyro_Y_RAW;
-    int16_t Gyro_Z_RAW;
-    double Gx;
-    double Gy;
-    double Gz;
+typedef struct{
+	I2C_HandleTypeDef *i2c_handle;
+	uint8_t device_address;
+	gyro_handle_t gyro;
+	accel_handle_t accel;
+	float temp;
+}mpu6050_handle_t;
 
-    float Temperature;
+#define MPU6050_I2C_DEFAULT_DEVICE_ADDRESS 0xD0
+/*
+ *  FS_SEL		   LSB
+ *    0			  131.0f
+ *    1			   65.5f
+ *    2			   32.8f
+ *    3			   16.4f
+ */
+#define MPU_GYRO_LSB 	131.0f
+/*
+ * AFS_SEL		   LSB
+ * 	  0			16384.0f
+ * 	  1			 8192.0f
+ * 	  2			 4096.0f
+ * 	  3			 2048.0f
+ */
+#define MPU_ACCEL_LSB	16384.0f
 
-    double KalmanAngleX;
-    double KalmanAngleY;
-} MPU6050_t;
+#define INIT_ALL	0x7F
+#define ACCEL_X		0x01
+#define ACCEL_Y		0x02
+#define ACCEL_Z		0x04
+#define ACCEL		0x07
+#define TEMP		0x08
+#define GYRO_X		0x10
+#define GYRO_Y		0x20
+#define GYRO_Z		0x40
+#define GYRO		0x70
 
-// Kalman structure
-typedef struct
-{
-    double Q_angle;
-    double Q_bias;
-    double R_measure;
-    double angle;
-    double bias;
-    double P[2][2];
-} Kalman_t;
+bool mpu6050_init(mpu6050_handle_t *handle, uint8_t setup);
+bool mpu6050_read_gyro(mpu6050_handle_t *handle);
+bool mpu6050_read_accel(mpu6050_handle_t *handle);
+bool mpu6050_read_temp(mpu6050_handle_t *handle);
+bool mpu6050_read_all(mpu6050_handle_t *handle);
 
-uint8_t MPU6050_Init(I2C_HandleTypeDef *I2Cx);
-
-void MPU6050_Read_Accel(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
-
-void MPU6050_Read_Gyro(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
-
-void MPU6050_Read_Temp(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
-
-void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct);
-
-double Kalman_getAngle(Kalman_t *Kalman, double newAngle, double newRate, double dt);
+#endif /* INC_MPU6050_H_ */
--- MPU6050/f103/Core/Src/mpu6050.c
+++ MPU6050/f103/Core/Src/mpu6050.c
@@ -1,238 +1,149 @@
 /*
  * mpu6050.c
  *
- *  Created on: Nov 13, 2019
- *      Author: Bulanov Konstantin
- *
- *  Contact information
- *  -------------------
- *
- * e-mail   :  leech001@gmail.com
+ *  Created on: Apr 13, 2025
+ *      Author: SergALLy
  */
-
-/*
- * |---------------------------------------------------------------------------------
- * | Copyright (C) Bulanov Konstantin,2021
- * |
- * | This program is free software: you can redistribute it and/or modify
- * | it under the terms of the GNU General Public License as published by
- * | the Free Software Foundation, either version 3 of the License, or
- * | any later version.
- * |
- * | This program is distributed in the hope that it will be useful,
- * | but WITHOUT ANY WARRANTY; without even the implied warranty of
- * | MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * | GNU General Public License for more details.
- * |
- * | You should have received a copy of the GNU General Public License
- * | along with this program.  If not, see <http://www.gnu.org/licenses/>.
- * |
- * | Kalman filter algorithm used from https://github.com/TKJElectronics/KalmanFilter
- * |---------------------------------------------------------------------------------
- */
-
-#include <math.h>
 #include "mpu6050.h"
 
-#define RAD_TO_DEG 57.295779513082320876798154814105
+typedef enum {
+	MPU_SMPRT_DIV =	0x19,		// Делитель частоты дискретизации
+	MPU_CONFIG = 0x1a,			// Конфигурация
+	MPU_GYRO_CONFIG	= 0x1b,		// Конфигурация гироскопа
+	MPU_ACCEL_CONFIG = 0x1c,	// Конфигурация акселерометра
+	MPU_FIFO_EN	= 0x23,			// Настройка выходных данных
+	MPU_INT_PIN_CFG = 0x37,		// Конфигурация включения пин INT
+	MPU_INT_ENABLE = 0x38,		// Разрешения прерываний
+	MPU_INT_STATUS = 0x3a,		// Состояние прерывания
+	MPU_ACCEL_XOUT_H = 0x3b,	// Измерения акселерометра
+	MPU_TEMP_OUT_H = 0x41,		// Измерение температуры
+	MPU_GYRO_XOUT_H = 0x43,		// Гироскопические измерения
+	MPU_USER_CTRL = 0x6a,		// Контроль пользователя
+	MPU_PWR_MGMT_1 = 0x6c,
+	MPU_PWR_MGMT_2 = 0x72,
+	MPU_FIFO_R_W = 0x74,
+	MPU_WHO_AM_I = 0x75
+} mpu6050_register_t;
 
-#define WHO_AM_I_REG 0x75
-#define PWR_MGMT_1_REG 0x6B
-#define SMPLRT_DIV_REG 0x19
-#define ACCEL_CONFIG_REG 0x1C
-#define ACCEL_XOUT_H_REG 0x3B
-#define TEMP_OUT_H_REG 0x41
-#define GYRO_CONFIG_REG 0x1B
-#define GYRO_XOUT_H_REG 0x43
+static bool mpu6050_read(mpu6050_handle_t *handle, uint8_t address, uint8_t *buff, uint8_t size)
+{
+	return HAL_I2C_Mem_Read(handle->i2c_handle, handle->device_address, address, I2C_MEMADD_SIZE_8BIT, buff, size, 100) == HAL_OK;
+}
 
-// Setup MPU6050
-#define MPU6050_ADDR 0xD0
-const uint16_t i2c_timeout = 100;
-const double Accel_Z_corrector = 14418.0;
-
-uint32_t timer;
-
-Kalman_t KalmanX = {
-    .Q_angle = 0.001f,
-    .Q_bias = 0.003f,
-    .R_measure = 0.03f};
-
-Kalman_t KalmanY = {
-    .Q_angle = 0.001f,
-    .Q_bias = 0.003f,
-    .R_measure = 0.03f,
+static bool mpu6050_write_u8(mpu6050_handle_t *handle, uint8_t address, uint8_t *data)
+{
+	return HAL_I2C_Mem_Write(handle->i2c_handle, handle->device_address, address, I2C_MEMADD_SIZE_8BIT, data, 1, 100) == HAL_OK;
 };
 
-uint8_t MPU6050_Init(I2C_HandleTypeDef *I2Cx)
+bool mpu6050_init(mpu6050_handle_t *handle, uint8_t setup)
 {
-    uint8_t check;
-    uint8_t Data;
+	bool success = true;
+	uint8_t data =0;
 
-    // check device ID WHO_AM_I
+	success &= mpu6050_read(handle, MPU_WHO_AM_I, &data, 1);
+	if (data == 0x68)
+	{
+		data = 0x00; // Внутренний генератор
+		success &= mpu6050_write_u8(handle, MPU_PWR_MGMT_1, &data);
 
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, WHO_AM_I_REG, 1, &check, 1, i2c_timeout);
+		data = 0x07; // Частота дискретизации 1кГц
+		success &= mpu6050_write_u8(handle, MPU_SMPRT_DIV, &data);
 
-    if (check == 0x68) // 0x68 will be returned by the sensor if everything goes well
-    {
-        // power management register 0X6B we should write all 0's to wake the sensor up
-        Data = 0;
-        HAL_I2C_Mem_Write(I2Cx, MPU6050_ADDR, PWR_MGMT_1_REG, 1, &Data, 1, i2c_timeout);
+		data = 0x00; // Без самопроверок, Диапазон +-250
+		success &= mpu6050_write_u8(handle, MPU_GYRO_CONFIG, &data);
 
-        // Set DATA RATE of 1KHz by writing SMPLRT_DIV register
-        Data = 0x07;
-        HAL_I2C_Mem_Write(I2Cx, MPU6050_ADDR, SMPLRT_DIV_REG, 1, &Data, 1, i2c_timeout);
+		data = 0x00; // Без самопроверок, диапазон +-2g
+		success &= mpu6050_write_u8(handle, MPU_ACCEL_CONFIG, &data);
 
-        // Set accelerometer configuration in ACCEL_CONFIG Register
-        // XA_ST=0,YA_ST=0,ZA_ST=0, FS_SEL=0 -> � 2g
-        Data = 0x00;
-        HAL_I2C_Mem_Write(I2Cx, MPU6050_ADDR, ACCEL_CONFIG_REG, 1, &Data, 1, i2c_timeout);
-
-        // Set Gyroscopic configuration in GYRO_CONFIG Register
-        // XG_ST=0,YG_ST=0,ZG_ST=0, FS_SEL=0 -> � 250 �/s
-        Data = 0x00;
-        HAL_I2C_Mem_Write(I2Cx, MPU6050_ADDR, GYRO_CONFIG_REG, 1, &Data, 1, i2c_timeout);
-        return 0;
-    }
-    return 1;
+		return success;
+	}
+	else
+		return false;
 }
 
-void MPU6050_Read_Accel(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct)
+bool mpu6050_read_gyro(mpu6050_handle_t *handle)
 {
-    uint8_t Rec_Data[6];
+	uint8_t data[6]={0};
+	int16_t buff = 0;
+	bool success = true;
 
-    // Read 6 BYTES of data starting from ACCEL_XOUT_H register
+	success &= mpu6050_read(handle, MPU_GYRO_XOUT_H, data, 6);
 
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 6, i2c_timeout);
+	buff = (int16_t)(data[0]<<8 | data[1]);
+	handle -> gyro.X = buff / MPU_GYRO_LSB;
 
-    DataStruct->Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
-    DataStruct->Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
-    DataStruct->Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
+	buff = (int16_t)(data[2]<<8 | data[3]);
+	handle -> gyro.Y = buff / MPU_GYRO_LSB;
 
-    /*** convert the RAW values into acceleration in 'g'
-         we have to divide according to the Full scale value set in FS_SEL
-         I have configured FS_SEL = 0. So I am dividing by 16384.0
-         for more details check ACCEL_CONFIG Register              ****/
+	buff = (int16_t)(data[4]<<8 | data[5]);
+	handle -> gyro.Z = buff / MPU_GYRO_LSB;
 
-    DataStruct->Ax = DataStruct->Accel_X_RAW / 16384.0;
-    DataStruct->Ay = DataStruct->Accel_Y_RAW / 16384.0;
-    DataStruct->Az = DataStruct->Accel_Z_RAW / Accel_Z_corrector;
+	return success;
 }
 
-void MPU6050_Read_Gyro(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct)
+bool mpu6050_read_accel(mpu6050_handle_t *handle)
 {
-    uint8_t Rec_Data[6];
+	uint8_t data[6]={0};
+	int16_t buff = 0;
+	bool success = true;
 
-    // Read 6 BYTES of data starting from GYRO_XOUT_H register
+	success &= mpu6050_read(handle, MPU_ACCEL_XOUT_H, data, 6);
 
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, GYRO_XOUT_H_REG, 1, Rec_Data, 6, i2c_timeout);
+	buff = (int16_t)(data[0]<<8 | data[1]);
+	handle -> accel.X = buff / MPU_ACCEL_LSB;
 
-    DataStruct->Gyro_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
-    DataStruct->Gyro_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
-    DataStruct->Gyro_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
+	buff = (int16_t)(data[2]<<8 | data[3]);
+	handle -> accel.Y = buff / MPU_ACCEL_LSB;
 
-    /*** convert the RAW values into dps (�/s)
-         we have to divide according to the Full scale value set in FS_SEL
-         I have configured FS_SEL = 0. So I am dividing by 131.0
-         for more details check GYRO_CONFIG Register              ****/
+	buff = (int16_t)(data[4]<<8 | data[5]);
+	handle -> accel.Z = buff / MPU_ACCEL_LSB;
 
-    DataStruct->Gx = DataStruct->Gyro_X_RAW / 131.0;
-    DataStruct->Gy = DataStruct->Gyro_Y_RAW / 131.0;
-    DataStruct->Gz = DataStruct->Gyro_Z_RAW / 131.0;
+	return success;
 }
 
-void MPU6050_Read_Temp(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct)
+bool mpu6050_read_temp(mpu6050_handle_t *handle)
 {
-    uint8_t Rec_Data[2];
-    int16_t temp;
+	uint8_t data[2]={0};
+	int16_t buff = 0;
+	bool success = true;
 
-    // Read 2 BYTES of data starting from TEMP_OUT_H_REG register
+	success &= mpu6050_read(handle, MPU_TEMP_OUT_H, data, 2);
 
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, TEMP_OUT_H_REG, 1, Rec_Data, 2, i2c_timeout);
+	buff = (int16_t)(data[0]<<8 | data[1]);
+	handle -> temp = buff/340.0f + 36.53f;
 
-    temp = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
-    DataStruct->Temperature = (float)((int16_t)temp / (float)340.0 + (float)36.53);
+	return success;
 }
 
-void MPU6050_Read_All(I2C_HandleTypeDef *I2Cx, MPU6050_t *DataStruct)
+bool mpu6050_read_all(mpu6050_handle_t *handle)
 {
-    uint8_t Rec_Data[14];
-    int16_t temp;
+	uint8_t data[14]={0};
+		int16_t buff = 0;
+		bool success = true;
 
-    // Read 14 BYTES of data starting from ACCEL_XOUT_H register
+		success &= mpu6050_read(handle, MPU_ACCEL_XOUT_H, data, 14);
 
-    HAL_I2C_Mem_Read(I2Cx, MPU6050_ADDR, ACCEL_XOUT_H_REG, 1, Rec_Data, 14, i2c_timeout);
+		buff = (int16_t)(data[0]<<8 | data[1]);
+		handle -> accel.X = buff / MPU_ACCEL_LSB;
 
-    DataStruct->Accel_X_RAW = (int16_t)(Rec_Data[0] << 8 | Rec_Data[1]);
-    DataStruct->Accel_Y_RAW = (int16_t)(Rec_Data[2] << 8 | Rec_Data[3]);
-    DataStruct->Accel_Z_RAW = (int16_t)(Rec_Data[4] << 8 | Rec_Data[5]);
-    temp = (int16_t)(Rec_Data[6] << 8 | Rec_Data[7]);
-    DataStruct->Gyro_X_RAW = (int16_t)(Rec_Data[8] << 8 | Rec_Data[9]);
-    DataStruct->Gyro_Y_RAW = (int16_t)(Rec_Data[10] << 8 | Rec_Data[11]);
-    DataStruct->Gyro_Z_RAW = (int16_t)(Rec_Data[12] << 8 | Rec_Data[13]);
+		buff = (int16_t)(data[2]<<8 | data[3]);
+		handle -> accel.Y = buff / MPU_ACCEL_LSB;
 
-    DataStruct->Ax = DataStruct->Accel_X_RAW / 16384.0;
-    DataStruct->Ay = DataStruct->Accel_Y_RAW / 16384.0;
-    DataStruct->Az = DataStruct->Accel_Z_RAW / Accel_Z_corrector;
-    DataStruct->Temperature = (float)((int16_t)temp / (float)340.0 + (float)36.53);
-    DataStruct->Gx = DataStruct->Gyro_X_RAW / 131.0;
-    DataStruct->Gy = DataStruct->Gyro_Y_RAW / 131.0;
-    DataStruct->Gz = DataStruct->Gyro_Z_RAW / 131.0;
+		buff = (int16_t)(data[4]<<8 | data[5]);
+		handle -> accel.Z = buff / MPU_ACCEL_LSB;
 
-    // Kalman angle solve
-    double dt = (double)(HAL_GetTick() - timer) / 1000;
-    timer = HAL_GetTick();
-    double roll;
-    double roll_sqrt = sqrt(
-        DataStruct->Accel_X_RAW * DataStruct->Accel_X_RAW + DataStruct->Accel_Z_RAW * DataStruct->Accel_Z_RAW);
-    if (roll_sqrt != 0.0)
-    {
-        roll = atan(DataStruct->Accel_Y_RAW / roll_sqrt) * RAD_TO_DEG;
-    }
-    else
-    {
-        roll = 0.0;
-    }
-    double pitch = atan2(-DataStruct->Accel_X_RAW, DataStruct->Accel_Z_RAW) * RAD_TO_DEG;
-    if ((pitch < -90 && DataStruct->KalmanAngleY > 90) || (pitch > 90 && DataStruct->KalmanAngleY < -90))
-    {
-        KalmanY.angle = pitch;
-        DataStruct->KalmanAngleY = pitch;
-    }
-    else
-    {
-        DataStruct->KalmanAngleY = Kalman_getAngle(&KalmanY, pitch, DataStruct->Gy, dt);
-    }
-    if (fabs(DataStruct->KalmanAngleY) > 90)
-        DataStruct->Gx = -DataStruct->Gx;
-    DataStruct->KalmanAngleX = Kalman_getAngle(&KalmanX, roll, DataStruct->Gx, dt);
+		buff = (int16_t)(data[6]<<8 | data[7]);
+		handle -> temp = buff/340.0f + 36.53f;
+
+		buff = (int16_t)(data[8]<<8 | data[9]);
+		handle -> gyro.X = buff / MPU_GYRO_LSB;
+
+		buff = (int16_t)(data[10]<<8 | data[11]);
+		handle -> gyro.Y = buff / MPU_GYRO_LSB;
+
+		buff = (int16_t)(data[12]<<8 | data[13]);
+		handle -> gyro.Z = buff / MPU_GYRO_LSB;
+
+		return success;
 }
 
-double Kalman_getAngle(Kalman_t *Kalman, double newAngle, double newRate, double dt)
-{
-    double rate = newRate - Kalman->bias;
-    Kalman->angle += dt * rate;
-
-    Kalman->P[0][0] += dt * (dt * Kalman->P[1][1] - Kalman->P[0][1] - Kalman->P[1][0] + Kalman->Q_angle);
-    Kalman->P[0][1] -= dt * Kalman->P[1][1];
-    Kalman->P[1][0] -= dt * Kalman->P[1][1];
-    Kalman->P[1][1] += Kalman->Q_bias * dt;
-
-    double S = Kalman->P[0][0] + Kalman->R_measure;
-    double K[2];
-    K[0] = Kalman->P[0][0] / S;
-    K[1] = Kalman->P[1][0] / S;
-
-    double y = newAngle - Kalman->angle;
-    Kalman->angle += K[0] * y;
-    Kalman->bias += K[1] * y;
-
-    double P00_temp = Kalman->P[0][0];
-    double P01_temp = Kalman->P[0][1];
-
-    Kalman->P[0][0] -= K[0] * P00_temp;
-    Kalman->P[0][1] -= K[0] * P01_temp;
-    Kalman->P[1][0] -= K[1] * P00_temp;
-    Kalman->P[1][1] -= K[1] * P01_temp;
-
-    return Kalman->angle;
-};
